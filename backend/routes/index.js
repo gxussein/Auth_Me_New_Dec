450ed72df7d145acb366fa867691d5dfa4677a48
Guled Hussein <gxussein@gmail.com>
2023-03-20 17:33:53 -0500

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+// GET /api/restore-user
+const { restoreUser } = require('../utils/auth.js');
+
+router.use(restoreUser);
+
+
+
+
+
+
+module.exports = router;
